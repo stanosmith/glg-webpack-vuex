9b83495d6c81e86ad0050e049d52d2ebea662724
Stan Smith <stan.smith@glg.com>
2018-02-01 15:24:38 -0700

    User Story #2938 - History mode enabled in Vue Router * Also, scroll to top on route change

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -5,11 +5,17 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     }
-  ]
+  ],
+  // Scroll to top of the page when changing routes
+  // scrollBehavior (to, from, savedPosition)
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  }
 })
